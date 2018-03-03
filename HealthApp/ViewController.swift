75d067744766a4bcbff5cc8d3b4bd6d644327716
David & Isabelle <Mac@MacBook-Air.local>
2018-03-03 12:00:41 -0600

    Initial Commit

---
+++ HealthApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthApp
+//
+//  Created by David & Isabelle on 3/3/18.
+//  Copyright © 2018 Ugo Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
